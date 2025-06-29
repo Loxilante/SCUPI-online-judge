5d9d51f7495be3afc6fdbd25ef9f822add29d400
Aquarii Beta <2451830400@qq.com>
2025-06-30 06:19:18 +0800

    refactor(aisb): remove obsolete files

--- backend/docker/ai_sandbox/gptapi/urls.py
+++
@@ -1,7 +0,0 @@
-# myapp/urls.py
-from django.urls import path
-from .views import chatgpt  # 导入你的视图函数
-
-urlpatterns = [
-    path('', chatgpt.as_view(), name='chatgpt'),  # 配置 'chatgpt/' 路由
-]
--- backend/docker/ai_sandbox/test.bat
+++
@@ -1,2 +0,0 @@
-start "" "http://localhost:5500/gpt-test.html"
-python -m http.server 5500"
